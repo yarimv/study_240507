f66b79f1a35b1ffb8dc2d5d542f8f60ae68fa13f
yarimv <39723219+yarimv@users.noreply.github.com>
2024-05-13 20:20:08 +0900

    ocr 성공

--- FaaS_test/apptest_test.py
+++ FaaS_test/apptest_test.py
@@ -7,10 +7,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from appium.webdriver.webdriver import AppiumBy
 from PIL import Image
 import pytesseract
+import re
 
 
 
-#환경변수 지정이 필요 할듯 ... 일단 ocr 되는거만 해보기
 
 
 #
@@ -33,18 +33,34 @@ import pytesseract
 #
 # time.sleep(5)
 
-file_name = "img.png"
+# 스크린샷에서 문자추출하는 method 로 구현하기
+
+
+
+file_name = "img2.png"
 # driver.save_screenshot(file_name)
-
-path = "/Users/ahrim/github/automation/FaaS_test/"+ file_name
-print("file name :", path )
+lang = 'kor+eng'
+path = "/Users/ahrimlee/work/Finda/FaaS_test/"+ file_name
 pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
-
-a = Image.open(path)
-result = pytesseract.image_to_string(a,lang='kor')
+korean_data_path = '/opt/homebrew/bin/tessdata/'
+#a = Image.open(path)
+result = pytesseract.image_to_string(path, lang=lang, config=f'--tessdata-dir "{korean_data_path}"')
 print(result)
 
 #ocr 로 숫자추출
+
+# 결과 문자열을 줄 단위로 분할
+lines = result.split('\n')
+# '메 시 지 에 서 가 져 옴' 다음 줄에 있는 문자열과 그 다음 줄의 숫자를 추출
+for i in range(len(lines)):
+    if '메 시 지 에 서 가 져 옴' in lines[i]:
+        if i + 1 < len(lines):  # 다음 줄이 있다면
+            next_line = lines[i + 1]
+            # 다음 줄에 있는 숫자 추출
+            numbers = re.findall(r'\d+', next_line)
+            if numbers:  # 숫자가 존재한다면
+                print(next_line)
+                break
 
 
 
