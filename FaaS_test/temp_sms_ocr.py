cddd6adbae1c991030cadc83a5bc48cfcb1ce69c
yarimv <yarimv@gmail.com>
2024-05-16 17:48:25 +0900

    ocr 성공

--- FaaS_test/temp_sms_ocr.py
+++ FaaS_test/temp_sms_ocr.py
@@ -40,12 +40,14 @@ import re
 file_name = "img2.png"
 # driver.save_screenshot(file_name)
 lang = 'kor+eng'
-path = "/Users/ahrimlee/work/Finda/FaaS_test/"+ file_name
+path = "/Users/ahrim/github/automation/FaaS_test/"+ file_name
 pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
 korean_data_path = '/opt/homebrew/bin/tessdata/'
 #a = Image.open(path)
 result = pytesseract.image_to_string(path, lang=lang, config=f'--tessdata-dir "{korean_data_path}"')
 print(result)
+
+
 
 #ocr 로 숫자추출
 
